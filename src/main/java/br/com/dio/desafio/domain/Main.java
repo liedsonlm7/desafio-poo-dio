4c601d7df28d9bd9cd8a83bce39f19850cf43f95
liedsonlm7 <liedsonlm7@gmail.com>
2025-12-29 16:40:50 -0300

    create entities

---
+++ src/main/java/br/com/dio/desafio/domain/Main.java
@@ -0,0 +1,31 @@
+package br.com.dio.desafio.domain;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descrição do curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("descrição do curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
+
+    }
+}
